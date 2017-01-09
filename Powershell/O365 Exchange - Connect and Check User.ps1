db0954b8c8ac6e02fc6092c001bf30f30bf9866d
Andrew Silkroski <andrew.silkroski@gmail.com>
2017-01-09 07:26:12 -0800

    Revised - CSV output and comments added

--- Powershell/O365 Exchange - Connect and Check User.ps1
+++ Powershell/O365 Exchange - Connect and Check User.ps1
@@ -10,11 +10,15 @@ Write-Host -ForegroundColor Cyan '----------------------------------------------
 Pause
 $UserCredential = Get-Credential -Message 'Please enter your Exchange Online/Office 365 credentials. This must be an administrator account.'
 $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+##Adding pause to make sure the session is connected before importing  - there is probably a better way to do it.##
+Pause
 Import-PSSession $Session
 $User = Read-Host -Prompt 'Enter the O365 Username you wish to check'
-Write-Host 'Running. You should see your results shortly.'
+Write-Host 'Running. Outputting to C:\Temp\List.csv'
 Write-Host -ForegroundColor Red -BackgroundColor Black 'Press Enter once this finishes to clean up the session.'
-Get-Mailbox | Get-MailboxPermission -User $User | Where {$_.user.tostring() -ne "NT AUTHORITY\SELF" -and $_.IsInherited -eq $false} | Format-List
+##ResultSize will add additional time but permit checking more than a thousand items and stop the error message. The path can also be modified for the CSV.
+##the previous version just showed in a list which wasn't useful for more than a quick look.
+Get-Mailbox -ResultSize Unlimited | Get-MailboxPermission -User $User | Where {$_.user.tostring() -ne "NT AUTHORITY\SELF" -and $_.IsInherited -eq $false} | Select-Object Identity,AccessRights,InheritanceType,IsValid,Deny,User,ObjectState | Export-CSV -path 'C:\Temp\List.csv'
 Pause
 Remove-PSSession $Session
 Write-Host 'Clean up complete. Please close this window.'
